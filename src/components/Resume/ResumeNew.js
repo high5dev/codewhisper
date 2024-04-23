dccfd7a7bc764f73daa5ecb888c3b7547bedfe7b
hibiotech <dr.eric1125@gmail.com>
2024-04-23 08:47:43 -0700

    cv changed

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-// import Particle from "../Particle";
-import pdf from "../../Assets/KennyStackerResume.pdf";
+import pdf from "../../Assets/cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -25,13 +24,6 @@ function ResumeNew() {
             <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
-        {/* <Row style={{ justifyContent: "center" }} >
-          <Pagination size="md">
-            <Pagination.Prev onClick={() => { if (pageNumber > 1) { setPageNumber(pageNumber - 1) } }} />
-            <Pagination.Item active>{pageNumber}</Pagination.Item>
-            <Pagination.Next onClick={() => { if (pageNumber < 3) { setPageNumber(pageNumber + 1) } }} />
-          </Pagination>
-        </Row> */}
         <Row style={{ justifyContent: "center", position: "relative" }} >
           <Button variant="primary" href={pdf} target="_blank" className="download-button">
             <AiOutlineDownload />
